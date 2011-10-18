47d81afa775e4ca9d212598952ca1b5eb97af443
Scott Gonyea <sgonyea@truecar.com>
2011-10-18 12:20:33 -0700

    Added more file patterns to the ignore list, wrt my malformed-whitespace fascism.

--- spec/quality_spec.rb
+++ spec/quality_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
-IGNORE = /\.(gitmodules|txt$|png$|tar$|gz$|rbc$|gem$|pdf$)/
+IGNORE = /\.(gitmodules|gitkeep$|gitignore$|txt$|png$|tar$|gz$|rbc$|gem$|pdf$)/
 
 describe "The application itself" do
-  xit "has no malformed whitespace" do
+  it "has no malformed whitespace" do
     files = `git ls-files`.split("\n").select {|fn| fn !~ IGNORE}
 
     files.should be_well_formed
