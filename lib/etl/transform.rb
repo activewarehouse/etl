39367190832a5f4dc0fa710731a9a4e4000e3913
Scott Gonyea <sgonyea@truecar.com>
2011-10-18 10:14:18 -0700

    Add Autoloads to Transform.

--- lib/etl/transform.rb
+++ lib/etl/transform.rb
@@ -1,6 +1,4 @@
-Dir[File.dirname(__FILE__) + "/transform/*.rb"].each { |file| require(file) }
-
-module ETL#:nodoc:
+module ETL #:nodoc:
   # Base class for transforms.
   #
   # A transform converts one value to another value using some sort of algorithm.
@@ -17,6 +15,23 @@ module ETL#:nodoc:
   #
   #   transform :sex, :decode, {:decode_table_path => 'delimited_decode.txt'}
   class Transform
+    autoload  :BlockTransform,              'etl/transform/block_transform'
+    autoload  :CalculationTransform,        'etl/transform/calculation_transform'
+    autoload  :DateToStringTransform,       'etl/transform/date_to_string_transform'
+    autoload  :DecodeTransform,             'etl/transform/decode_transform'
+    autoload  :DefaultTransform,            'etl/transform/default_transform'
+    autoload  :ForeignKeyLookupTransform,   'etl/transform/foreign_key_lookup_transform'
+    autoload  :HierarchyLookupTransform,    'etl/transform/hierarchy_lookup_transform'
+    autoload  :Md5Transform,                'etl/transform/md5_transform'
+    autoload  :OrdinalizeTransform,         'etl/transform/ordinalize_transform'
+    autoload  :Sha1Transform,               'etl/transform/sha1_transform'
+    autoload  :SplitFieldsTransform,        'etl/transform/split_fields_transform'
+    autoload  :StringToDateTimeTransform,   'etl/transform/string_to_date_time_transform'
+    autoload  :StringToDateTransform,       'etl/transform/string_to_date_transform'
+    autoload  :StringToTimeTransform,       'etl/transform/string_to_time_transform'
+    autoload  :TrimTransform,               'etl/transform/trim_transform'
+    autoload  :TypeTransform,               'etl/transform/type_transform'
+
     class << self
       # Transform the specified value using the given transforms. The transforms can either be
       # Proc objects or objects which extend from Transform and implement the method <tt>transform(value)</tt>.
